96d7696051566440a39421eb5a1a37e3ef3f8ee2
HtunTaukOo <htuntaukoo86@gmail.com>
2026-02-13 14:16:20 +0700

    initial commit for user profile frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import TestApi from "./TestApi";
 import UserManagement from "./pages/UserManagement";
 import UserEdit from "./pages/UserEdit";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/test_api" element={<TestApi />} />
       <Route path="/user" element={<UserManagement />} />
       <Route path="/user/:id" element={<UserEdit />} />
+      <Route path="/profile/:id" element={<UserProfile />} />
     </Routes>
   );
 }
